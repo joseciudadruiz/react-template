fd2143171b556fe3103865b27f6bde17c14948de
joseciudadruiz <joseciudadruiz@gmail.com>
2021-01-14 16:21:52 +0000

    template in react

---
+++ index.js
@@ -0,0 +1,15 @@
+
+//App//
+
+const App = () => {
+  return (
+    <Template>
+      <h1 id="content">Main Content</h1>
+    </Template>
+  );
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
